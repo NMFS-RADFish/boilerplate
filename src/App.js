c7c38a398a2e3d3862e01b0acd14475c2747bf5d
nick gambino <contact@nickgambino.dev>
2023-12-05 14:44:30 -0700

    Add offline indicator

--- src/App.js
+++ src/App.js
@@ -3,7 +3,46 @@ import "./App.css";
 import RADForm from "./components/RADForm.js";
 
 function App() {
-  return <RADForm />;
+  const [onlineStatus, setOnlineStatus] = useState(false);
+
+  // Check if the app is offline
+  const isOffline = !navigator.onLine;
+
+  useEffect(() => {
+    const handleOnline = () => {
+      // You may want to refetch data when the app comes online
+      // You can do that by using the refetch function from the useQuery hook
+      setOnlineStatus(true);
+    };
+
+    const handleOffline = () => {
+      setOnlineStatus(false);
+    };
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    // need this cleanup, else event listeners are immediately removed
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, [isOffline]);
+
+  return (
+    <div className="App">
+      <Alert
+        type={onlineStatus ? "success" : "error"}
+        headingLevel={"h1"}
+        hidden={false}
+      >
+        {onlineStatus ? "Application online!" : "Application currently offline"}
+      </Alert>
+      <main>
+        <RADForm />
+      </main>
+    </div>
+  );
 }
 
 export default App;
