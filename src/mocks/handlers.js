540d8193235f1233512f85d25cd790931a071cc6
mellanie <mellaniesoriano@gmail.com>
2024-02-07 11:05:31 -0800

    Save form data when offline

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -10,6 +10,28 @@ export const handlers = [
   }),
   http.post(MSW_ENDPOINT.SPECIES, async ({ request }) => {
     const response = await request.json();
-    return HttpResponse.json({ data: response }, { status: 201 });
+
+    if (!navigator.onLine) {
+      const formData = new FormData();
+      formData.append("timestamp", Date.now());
+
+      for (let key in response) {
+        formData.append(key, response[key]);
+      }
+
+      localStorage.getItem("formData")
+        ? localStorage.setItem(
+            "formData",
+            JSON.stringify([
+              ...JSON.parse(localStorage.getItem("formData")),
+              Object.fromEntries(formData),
+            ]),
+          )
+        : localStorage.setItem("formData", JSON.stringify([Object.fromEntries(formData)]));
+
+      return HttpResponse.error(null, { status: 500 });
+    } else {
+      return HttpResponse.json({ data: response }, { status: 201 });
+    }
   }),
 ];
