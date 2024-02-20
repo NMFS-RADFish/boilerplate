eb668e6ebfe917fb5aa626e9f9c71d3551c0061e
mellanie <mellaniesoriano@gmail.com>
2024-02-20 15:18:20 -0800

    Add offline data storage docs

--- src/storage/LocalStorageMethod.js
+++ src/storage/LocalStorageMethod.js
@@ -1,13 +1,26 @@
 import { generateUUID } from "../utilities";
 import { StorageMethod } from "./StorageMethod";
 
+/**
+ * Class representing a local storage method.
+ * @extends StorageMethod
+ */
 export class LocalStorageMethod extends StorageMethod {
+  /**
+   * Create a new local storage method.
+   * @param {string} key - The key to use for storing data in localStorage, e.g. "formData".
+   */
   constructor(key) {
     super();
     this.key = key;
     this.store = localStorage.getItem(this.key);
   }
 
+  /**
+   * Create and store data in local storage.
+   * @param {Object} data - The data to store, e.g. { numberOfFish: "1", species: "Grouper" }.
+   * @throws {Error} If an error occurs while stringifying the data.
+   */
   create(data) {
     const existingData = JSON.parse(this.store) || [];
     const uuid = generateUUID();
@@ -20,6 +33,12 @@ export class LocalStorageMethod extends StorageMethod {
     }
   }
 
+  /**
+   * Find data in local storage.
+   * @param {Object} criteria - The criteria to use for finding data, e.g. { uuid: "1234" }.
+   * @return {Array} The found data, e.g. [ ["uuid-1234", { numberOfFish: "1", species: "Grouper" }] ].
+   * @throws {Error} If an error occurs while parsing the data.
+   */
   find(criteria) {
     try {
       const parsedData = JSON.parse(this.store);
@@ -40,6 +59,13 @@ export class LocalStorageMethod extends StorageMethod {
     }
   }
 
+  /**
+   * Update data in local storage.
+   * @param {Object} criteria - The criteria to use for updating data, e.g. { uuid: "1234" }.
+   * @param {Object} data - The new data, e.g. { numberOfFish: "1", species: "Grouper"}.
+   * @return {Array} The updated data.
+   * @throws {Error} If an error occurs while stringifying the data.
+   */
   update(criteria, data) {
     try {
       const originalData = this.find(this.key);
--- src/storage/StorageModel.js
+++ src/storage/StorageModel.js
@@ -1,23 +1,52 @@
+/**
+ * Class representing a factory for creating storage models.
+ */
 export class StorageModelFactory {
+  /**
+   * Create a new storage model.
+   * @param {StorageMethod} storageMethod - The storage method to use (`LocalStorageMethod` or `IndexedDBMethod`).
+   * @return {StorageModel} The created storage model.
+   */
   static createModel(storageMethod) {
     return new StorageModel(storageMethod);
   }
 }
 
+/**
+ * Class representing a storage model.
+ */
 export class StorageModel {
+  /**
+   * Create a new storage model.
+   * @param {StorageMethod} storageMethod - The storage method to use (LocalStorageMethod or IndexedDBMethod).
+   */
   constructor(storageMethod) {
     this.storageMethod = storageMethod;
   }
 
+  /**
+   * Create data in the storage.
+   * @param {Object} data - The data to create, e.g. { numberOfFish: 1, species: "Grouper" }.
+   */
   create(data) {
     this.storageMethod.create(data);
   }
+
+  /**
+   * Find data in the storage.
+   * @param {Object} criteria - The criteria to use for finding data, e.g. { uuid: "1234" }.
+   * @return {Array|Promise<Array>} The found data.
+   */
   find(criteria) {
     return this.storageMethod.find(criteria);
   }
-  // loadEntryByUUID(uuid) {
-  //   return this.storageMethod.loadOne(uuid);
-  // }
+
+  /**
+   * Update data in the storage.
+   * @param {Object} criteria - The criteria to use for updating data, e.g. { uuid: "1234" }.
+   * @param {Object} data - The new data, e.g. { numberOfFish: 2, species: "Grouper" }.
+   * @return {Array|Promise<Array>} The updated data.
+   */
   update(criteria, data) {
     return this.storageMethod.update(criteria, data);
   }
