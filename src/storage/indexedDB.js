de048832d77de2803690dfd4047a2a0acedad654
IsaiahHarris <isaiah.harris.hi@gmail.com>
2024-02-23 09:24:17 -1000

    update env var

--- src/storage/indexedDB.js
+++ src/storage/indexedDB.js
@@ -2,7 +2,7 @@ import Dexie from "dexie";
 
 export const db = new Dexie(process.env.REACT_APP_INDEXED_DB_NAME);
 
-db.version(1).stores({
+db.version(process.env.REACT_APP_INDEXED_DB_VERSION).stores({
   formData:
     "uuid, fullName, email, phoneNumber, numberOfFish, address1, address2, city, state, zipcode, occupation, department, species, computedPrice",
 });
