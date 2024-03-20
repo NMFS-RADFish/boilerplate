82988a407c7b323139aaf77c3860ea260dfcbae0
mellanie <mellaniesoriano@gmail.com>
2024-03-20 13:53:24 -0700

    Add delete method for indexedDB

--- src/storage/IndexedDBMethod.js
+++ src/storage/IndexedDBMethod.js
@@ -25,6 +25,7 @@ export class IndexedDBMethod extends StorageMethod {
   /**
    * Create and store data in IndexedDB.
    * @param {Object} data - The data to store, e.g. { numberOfFish: "1", species: "Grouper" }.
+   * @return {Promise<Object>} A promise that resolves to the found data as an object.
    * @throws {Error} If an error occurs while adding the data to IndexedDB.
    */
   async create(data) {
@@ -58,7 +59,7 @@ export class IndexedDBMethod extends StorageMethod {
 
   /**
    * Update data in IndexedDB.
-   * @param {Object} criteria - The criteria to use for updating data, e.g. { uuid: "1234" .
+   * @param {Object} criteria - The criteria to use for updating data, e.g. { uuid: "1234" }.
    * @param {Object} data - The new data, e.g. { numberOfFish: "1", species: "Grouper" }.
    * @return {Promise<Array>} A promise that resolves to the updated data.
    * @throws {Error} If an error occurs while updating the data in IndexedDB.
@@ -66,6 +67,21 @@ export class IndexedDBMethod extends StorageMethod {
   async update(criteria, data) {
     try {
       return await this.db.formData.put(data, criteria.uuid);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * Delete data in IndexedDB.
+   * @param {Array} UUIDs - Array of UUIDs to delete data, e.g. ["uuid123", "uuid321"].
+   * @return {Promise<Boolean>} A promise that resolves to `true` when the data is deleted.
+   * @throws {Error} If an error occurs while deleting the data in IndexedDB.
+   */
+  async delete(uuids) {
+    try {
+      await this.db.formData.bulkDelete(uuids);
+      return true;
     } catch (error) {
       throw error;
     }
