dff15525097581a1d2eb23bf088e3210145c8e64
mellanie <mellaniesoriano@gmail.com>
2024-03-20 15:40:22 -0700

    Moved indexedDB config to IndexedDBMethod

--- src/storage/indexedDB.js
+++
@@ -1,8 +0,0 @@
-// import Dexie from "dexie";
-
-// export const db = new Dexie(import.meta.env.VITE_INDEXED_DB_NAME);
-
-// db.version(import.meta.env.VITE_INDEXED_DB_VERSION).stores({
-//   formData:
-//     "uuid, fullName, email, phoneNumber, numberOfFish, address1, address2, city, state, zipcode, occupation, department, species, computedPrice",
-// });
