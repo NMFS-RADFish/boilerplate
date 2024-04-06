768d41b74dab79481947b84ece203e25be7f4d46
IsaiahHarris <isaiah.harris.hi@gmail.com>
2024-04-06 08:47:34 -1000

    remove error message

--- src/react-radfish/inputs/index.jsx
+++ src/react-radfish/inputs/index.jsx
@@ -24,7 +24,7 @@ const RadfishInputLabel = (props) => {
 };
 
 const RadfishErrorMessage = (props) => {
-  return <TwErrorMessage {...props} className={`radfish-error-message ${props.className || ""}`} />;
+  return <TwErrorMessage {...props} className={`${props.className || ""}`} />;
 };
 
 export {
