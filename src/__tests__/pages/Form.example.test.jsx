4d5a06e182365259ab4c92b03f598a74002fddc3
nick gambino <contact@nickgambino.dev>
2024-05-09 16:57:07 -0600

    Update Form unit test

--- src/__tests__/pages/Form.example.test.jsx
+++ src/__tests__/pages/Form.example.test.jsx
@@ -3,47 +3,56 @@ import { render, fireEvent } from "@testing-library/react";
 import * as formWrapper from "../../contexts/FormWrapper.example";
 import { Form } from "../../pages/Form.example";
 import * as offlineWrapper from "../../packages/contexts/OfflineStorageWrapper";
+import * as reactRouter from "react-router-dom";
 
-// Mocking react-router-dom hooks
-vi.mock("react-router-dom", async () => ({
-  ...(await vi.importActual("react-router-dom")),
-  useNavigate: vi.fn(),
-  useSearchParams: vi.fn(() => [new URLSearchParams(), vi.fn()]),
-}));
-
-vi.mock("../../contexts/FormWrapper", async () => {
+vi.mock("react-router-dom", async () => {
+  const actualReactRouterDom = await vi.importActual("react-router-dom");
   return {
-    ...(await vi.importActual("../../contexts/FormWrapper")),
-    useFormState: vi.fn(),
+    ...actualReactRouterDom,
+    useNavigate: () => vi.fn(),
+    useSearchParams: () => [new URLSearchParams(), vi.fn()],
   };
 });
 
-vi.mock("../../packages/contexts/OfflineStorageWrapper.jsx", async () => {
+function setupMocks() {
+  const mockedHandleChange = vi.fn();
+  const mockedHandleBlur = vi.fn();
+  const mockedUseFormState = vi.fn(() => ({
+    formData: {},
+    validationErrors: {},
+    handleChange: mockedHandleChange,
+    handleBlur: mockedHandleBlur,
+    visibleInputs: {},
+  }));
+  const mockedCreateOfflineData = vi.fn();
+  const mockedUseOfflineStorage = vi.fn(() => ({
+    updateOfflineData: vi.fn(),
+    createOfflineData: mockedCreateOfflineData,
+    findOfflineData: vi.fn(() => Promise.resolve([])),
+  }));
+
   return {
-    ...(await vi.importActual("../../packages/contexts/OfflineStorageWrapper.jsx")),
-    useOfflineStorage: vi.fn(),
+    mockedHandleChange,
+    mockedHandleBlur,
+    mockedUseFormState,
+    mockedCreateOfflineData,
+    mockedUseOfflineStorage,
   };
-});
+}
 
 describe("Form", () => {
-  it("renders and fires events", async () => {
-    const mockedHandleChange = vi.fn();
-    const mockedHandleBlur = vi.fn();
-    const mockedUseFormState = vi.fn(() => ({
-      formData: {},
-      validationErrors: {},
-      handleChange: mockedHandleChange,
-      handleBlur: mockedHandleBlur,
-      visibleInputs: {},
-    }));
-    const mockedUseOfflineStorage = vi.fn(() => {
-      return {
-        updateOfflineData: vi.fn(),
-      };
-    });
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+  });
+
+  it("renders and fires events with id", async () => {
+    const { mockedHandleBlur, mockedUseFormState, mockedUseOfflineStorage } = setupMocks();
+    const mockedkUseParams = () => ({ id: "123" });
 
     vi.spyOn(formWrapper, "useFormState").mockImplementation(mockedUseFormState);
     vi.spyOn(offlineWrapper, "useOfflineStorage").mockImplementation(mockedUseOfflineStorage);
+    vi.spyOn(reactRouter, "useParams").mockImplementation(mockedkUseParams);
 
     const { getByTestId } = render(
       <formWrapper.FormWrapper>
@@ -52,10 +61,27 @@ describe("Form", () => {
     );
 
     const inputElement = getByTestId("inputId");
-
     fireEvent.change(inputElement, { target: { value: "Mock Input Value" } });
-    expect(mockedHandleChange).toHaveBeenCalled();
     fireEvent.blur(inputElement);
     expect(mockedHandleBlur).toHaveBeenCalled();
+  });
+
+  it("renders and fires events without id", async () => {
+    const { mockedUseFormState, mockedCreateOfflineData, mockedUseOfflineStorage } = setupMocks();
+    const mockedkUseParams = () => ({});
+
+    vi.spyOn(formWrapper, "useFormState").mockImplementation(mockedUseFormState);
+    vi.spyOn(offlineWrapper, "useOfflineStorage").mockImplementation(mockedUseOfflineStorage);
+    vi.spyOn(reactRouter, "useParams").mockImplementation(mockedkUseParams);
+
+    const { getByTestId } = render(
+      <formWrapper.FormWrapper>
+        <Form />
+      </formWrapper.FormWrapper>,
+    );
+
+    const inputElement = getByTestId("init-complex");
+    fireEvent.click(inputElement);
+    expect(mockedCreateOfflineData).toHaveBeenCalled();
   });
 });
