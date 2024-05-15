f54aab2d86d485622b469eba9d7e967cee0f87df
mellanie <mellaniesoriano@gmail.com>
2024-05-15 13:24:56 -0700

    Add annotation

--- examples/on-device-storage/src/App.jsx
+++ examples/on-device-storage/src/App.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useOfflineStorage } from "./packages/contexts/OfflineStorageWrapper";
-import { Button, FormGroup, Label, TextInput } from "@trussworks/react-uswds";
+import {
+  Button,
+  FormGroup,
+  Label,
+  TextInput,
+  Alert,
+} from "@trussworks/react-uswds";
 
 const App = () => {
   const [formData, setFormData] = useState([]);
@@ -68,7 +74,32 @@ const App = () => {
 
   return (
     <div className="grid-container">
-      <h1>On Device Storage</h1>
+      <h1>On Device Storage Example</h1>
+      <Alert type="info" heading="Information" headingLevel="h2">
+        This is an example of how to use the `OfflineStorageWrapper` context and
+        the provided `useOfflineStorage` hook to interact with on-device
+        storage. This example demonstrates how to create, read, update, and
+        delete data from IndexedDB. The `useOfflineStorage` hooks uses Dexie.js
+        under the hood.
+        <br />
+        <br />
+        <a
+          href="https://nmfs-radfish.github.io/documentation"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button type="button">Go To Documentation</Button>
+        </a>
+        <a
+          href="https://dexie.org/docs/Tutorial/Getting-started"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button type="button">Dexie Docs</Button>
+        </a>
+      </Alert>
+      <br />
+      <br />
       <Button type="submit" onClick={(e) => createData(e)}>
         Create Data
       </Button>
