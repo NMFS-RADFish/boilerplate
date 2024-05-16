50fdd08749e64477ac779e7fc34440e637767f9a
mellanie <mellaniesoriano@gmail.com>
2024-05-16 14:20:21 -0700

    Add annotation

--- examples/network-status/src/App.jsx
+++ examples/network-status/src/App.jsx
@@ -20,6 +20,17 @@ function App() {
   return (
     <div className="grid-container">
       <Toast toast={toast} />
+      <Alert type="info" headingLevel={"h1"} heading="Network Status Example">
+        This is an example of a network status indicator. The application will display a toast
+        notification for 5 seconds when network is offline.
+        <a
+          href="https://nmfs-radfish.github.io/documentation/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button type="button">Go To Documentation</Button>
+        </a>
+      </Alert>
       <h1>Network Status Example</h1>
       <h3>Network Status: {isOffline ? "Offline" : "Online"}</h3>
     </div>
