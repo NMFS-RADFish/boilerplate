7b124583791812b37bd9d56d42b80d80ede055a8
nick gambino <contact@nickgambino.dev>
2024-05-23 15:07:19 -0600

    Remove uneccesary annotation

--- examples/computed-form-fields/src/App.jsx
+++ examples/computed-form-fields/src/App.jsx
@@ -24,11 +24,6 @@ const FormInfoAnnotation = () => {
   return (
     <Alert type="info" headingLevel={"h1"} heading="Example Form">
       This is an example of a form with form inputs that compute the values of other form inputs.
-      <br />
-      <br />
-      <strong>Note:</strong> Annotations are for informational purposes only. In production, you
-      would remove the annotations. Components with annotations above them are optional. You can
-      choose whether or not to use them in your application.
     </Alert>
   );
 };
