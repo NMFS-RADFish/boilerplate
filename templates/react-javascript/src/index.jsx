f4d6436f71a19b4df5ba20eea03bea6bb753f5bb
Tony Gaskell <tony@sudokrew.com>
2024-06-03 11:09:49 -1000

    Update npm references

--- templates/react-javascript/src/index.jsx
+++ templates/react-javascript/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/theme.css";
 import App from "./App";
-import { Application } from "../../../packages/radfish";
+import { Application } from "@nmfs-radfish/radfish";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
