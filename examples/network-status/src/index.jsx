4db2b1ea6fe6aa5b100dae64edf57e47661801a0
Jesse Copeland <jesse.copeland@noaa.gov>
2024-06-05 12:09:23 -1000

    Add service worker to network-status-example

--- examples/network-status/src/index.jsx
+++ examples/network-status/src/index.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/theme.css";
 import App from "./App";
+import { Application } from "@nmfs-radfish/radfish";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const app = new Application({
+  serviceWorker: {
+    url: "/service-worker.js",
+  }
+});
+
+app.on("ready", () => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
