4279730eed9f6dbc29dd9d4d0b46b1d16b0df240
nick gambino <contact@nickgambino.dev>
2024-06-05 13:50:14 -0700

    Move Form Annotation to App.jsx

--- examples/simple-form/src/App.jsx
+++ examples/simple-form/src/App.jsx
@@ -1,14 +1,50 @@
 import "./index.css";
-import SimpleForm, { SimpleFormDetails } from "./pages/Form";
+import SimpleForm from "./pages/Form";
+import { Alert, Link, Button } from "@trussworks/react-uswds";
 
 function App() {
   return (
-    <div className="grid-container">
-      <h1>Simple Form Examples</h1>
-      <SimpleForm />
-      <SimpleFormDetails />
+    <div className="App">
+      <div className="grid-container">
+        <FormInfoAnnotation />
+        <Router>
+          <Routes>
+            <Route path="/" element={<SimpleForm />} />
+            <Route path="/:id" element={<SimpleForm />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
+
+const FormInfoAnnotation = () => {
+  return (
+    <Alert type="info" heading="Persisted Form Example" headingLevel="h1">
+      This is an example of a form with details coming from IndexedDB. The form data is stored in
+      the browser's IndexedDB using methods from the `useOfflineStorage` hook, which uses Dexie.js
+      behind the scenes.
+      <br />
+      <br />
+      Please note that the form below will only populate when you have data saved in IndexedDB.
+      <br />
+      <br />
+      <Link
+        href="https://nmfs-radfish.github.io/documentation"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button type="button">Go To Documentation</Button>
+      </Link>
+      <Link
+        href="https://dexie.org/docs/Tutorial/Getting-started"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button type="button">Dexie Docs</Button>
+      </Link>
+    </Alert>
+  );
+};
 
 export default App;
--- examples/simple-form/src/pages/Form.jsx
+++ examples/simple-form/src/pages/Form.jsx
@@ -3,74 +3,7 @@ import React, { useEffect, useState } from "react";
 import { FormGroup, Alert, Link, TextInput, Label, Button, Form } from "@trussworks/react-uswds";
 import { useOfflineStorage } from "../packages/contexts/OfflineStorageWrapper";
 
-function SimpleForm() {
-  const { createOfflineData } = useOfflineStorage();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const values = {};
-
-    for (let [key, value] of formData.entries()) {
-      values[key] = value;
-    }
-
-    createOfflineData("formData", values);
-    // Handle form submission, usually by sending a POST request to a server
-    // Example: fetch.post("/api/form", values)
-    console.log("Form submitted");
-  };
-  return (
-    <Form
-      className="maxw-full margin-205 padding-205 bg-white radius-8px shadow-2"
-      onSubmit={handleSubmit}
-    >
-      <Alert type="info" heading="Unpersisted Form Example" headingLevel="h1">
-        This is a simple form example. The form data is stored with the React state.
-        <br />
-        <br />
-        <Link
-          href="https://nmfs-radfish.github.io/documentation"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button type="button">Go To Documentation</Button>
-        </Link>
-        <Link
-          href="https://dexie.org/docs/Tutorial/Getting-started"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button type="button">Dexie Docs</Button>
-        </Link>
-      </Alert>
-      <div className="margin-bottom-2">
-        <FormGroup>
-          <Label className="text-bold" htmlFor="fullName">
-            Name
-          </Label>
-          <TextInput id="fullName" name="fullName" type="text" />
-          <Label className="text-bold" htmlFor="numberOfFish">
-            Number of Fish
-          </Label>
-          <TextInput id="numberOfFish" name="numberOfFish" type="number" />
-          <Label className="text-bold" htmlFor="species">
-            Species
-          </Label>
-          <TextInput id="species" name="species" type="text" />
-          <Label className="text-bold" htmlFor="computedPrice">
-            Price
-          </Label>
-          <TextInput id="computedPrice" name="computedPrice" type="number" />
-          <Button type="submit" className="margin-top-2">
-            Submit
-          </Button>
-        </FormGroup>
-      </div>
-    </Form>
-  );
-}
-
-export function SimpleFormDetails() {
+export function SimpleForm() {
   const [formData, setFormData] = useState({});
   const { findOfflineData, createOfflineData } = useOfflineStorage();
 
@@ -120,31 +53,6 @@ export function SimpleFormDetails() {
       className="maxw-full margin-205 padding-205 bg-white radius-8px shadow-2"
       onSubmit={handleSubmit}
     >
-      <Alert type="info" heading="Persisted Form Example" headingLevel="h1">
-        This is an example of a form with details coming from IndexedDB. The form data is stored in
-        the browser's IndexedDB using methods from the `useOfflineStorage` hook, which uses Dexie.js
-        behind the scenes.
-        <br />
-        <br />
-        Please note that the form below will only populate when you have data saved in IndexedDB.
-        <br />
-        <br />
-        <Link
-          href="https://nmfs-radfish.github.io/documentation"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button type="button">Go To Documentation</Button>
-        </Link>
-        <Link
-          href="https://dexie.org/docs/Tutorial/Getting-started"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button type="button">Dexie Docs</Button>
-        </Link>
-      </Alert>
-
       <FormGroup>
         <Label className="text-bold" htmlFor="fullName">
           Name
