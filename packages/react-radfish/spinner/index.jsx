29b37c933b14fc980d5fd94c97117c3c1dd40b9e
nick gambino <contact@nickgambino.dev>
2024-06-12 13:13:23 -0700

    Add default props

--- packages/react-radfish/spinner/index.jsx
+++ packages/react-radfish/spinner/index.jsx
@@ -1,6 +1,11 @@
 import "./style.css";
 
-export const Spinner = ({ color, width, height, stroke }) => {
+export const Spinner = ({
+  color = "#0093d0",
+  width = 50,
+  height = 50,
+  stroke = 8,
+}) => {
   return (
     <div
       class="loading"
