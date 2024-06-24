5ff03245e82ce619a0107a6bd788aefaa83ae5c4
Tony Gaskell <tony@sudokrew.com>
2024-06-24 11:50:57 -1000

    Remove react plugins from vite config

--- packages/react-radfish/vite.config.js
+++ packages/react-radfish/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
-// import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
-    // react(),
   ],
   test: {
     globals: true,
