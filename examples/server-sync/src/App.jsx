3ce0de051e110aefdb76774fafec97196a1bd03d
IsaiahHarris <isaiah.harris.hi@gmail.com>
2024-07-01 11:21:14 -1000

    remove layout

--- examples/server-sync/src/App.jsx
+++ examples/server-sync/src/App.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Button, Alert, Link } from "@trussworks/react-uswds";
 
-import Layout from "./components/Layout";
-
 import { ServerSync } from "./components/ServerSync";
 
 function App() {
@@ -13,11 +11,9 @@ function App() {
     // Remove header
     <div className="App">
       <Router>
-        <Layout>
-          <h1>Server Sync Example</h1>
-          <InfoAnnotation />
-          <ServerSync />
-        </Layout>
+        <h1>Server Sync Example</h1>
+        <InfoAnnotation />
+        <ServerSync />
       </Router>
     </div>
   );
