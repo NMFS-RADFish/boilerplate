6ae85a2ad6b88b69ed8d1cad22eaf767f6692052
IsaiahHarris <isaiah.harris.hi@gmail.com>
2024-07-31 10:41:51 -1000

    update label and hint text to be in parent

--- packages/react-radfish/DatePicker/index.jsx
+++ packages/react-radfish/DatePicker/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-export const DatePicker = ({ type = "date", min, max, selectedDate, setSelectedDate }) => {
-  const hintText = {
-    date: "mm/dd/yyyy",
-    time: "hh:mm",
-    "datetime-local": "mm/dd/yyyy hh:mm",
-  }[type];
-
+export const DatePicker = ({
+  type = "date",
+  hintText,
+  label,
+  min,
+  max,
+  selectedDate,
+  setSelectedDate,
+}) => {
   return (
     <div className="usa-date-range-picker">
       <div className="usa-form-group">
         <label className="usa-label" id="date-start-label" htmlFor="date-start">
-          Start date
+          {label}
         </label>
         <div className="usa-hint" id="date-start-hint">
           {hintText}
