54134f084f0f2b98db8bc6c11738889720d9fd4b
IsaiahHarris <isaiah.harris.hi@gmail.com>
2024-07-31 11:07:28 -1000

    remove date range picker

--- packages/react-radfish/DatePicker/index.jsx
+++ packages/react-radfish/DatePicker/index.jsx
@@ -10,30 +10,28 @@ export const DatePicker = ({
   setSelectedDate,
 }) => {
   return (
-    <div className="usa-date-range-picker">
-      <div className="usa-form-group">
-        <label className="usa-label" id="date-start-label" htmlFor="date-start">
-          {label}
-        </label>
-        <div className="usa-hint" id="date-start-hint">
-          {hintText}
-        </div>
-        <div className="usa-date-picker">
-          <input
-            type={type}
-            onChange={(e) => {
-              setSelectedDate(e.target.value);
-            }}
-            value={selectedDate}
-            className="usa-input"
-            id="date-start"
-            name="date-start"
-            aria-labelledby="date-start-label"
-            aria-describedby="date-start-hint"
-            min={min}
-            max={max}
-          />
-        </div>
+    <div className="usa-form-group">
+      <label className="usa-label" id="date-start-label" htmlFor="date-start">
+        {label}
+      </label>
+      <div className="usa-hint" id="date-start-hint">
+        {hintText}
+      </div>
+      <div className="usa-date-picker">
+        <input
+          type={type}
+          onChange={(e) => {
+            setSelectedDate(e.target.value);
+          }}
+          value={selectedDate}
+          className="usa-input"
+          id="date-start"
+          name="date-start"
+          aria-labelledby="date-start-label"
+          aria-describedby="date-start-hint"
+          min={min}
+          max={max}
+        />
       </div>
     </div>
   );
