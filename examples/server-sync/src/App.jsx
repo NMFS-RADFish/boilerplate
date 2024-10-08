ea13b8ca29c93fac13f9028476474645c35b90c2
Jay Giang <jayhgiang@gmail.com>
2024-10-08 15:38:00 -0700

    Update server sync

--- examples/server-sync/src/App.jsx
+++ examples/server-sync/src/App.jsx
@@ -2,22 +2,22 @@ import "./index.css";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Button, Alert, Link, GridContainer } from "@trussworks/react-uswds";
-
+import { Application } from "@nmfs-radfish/react-radfish";
 import { ServerSync } from "./components/ServerSync";
 
 function App() {
   return (
-    // Remove app div
-    // Remove header
-    <GridContainer>
-      <div className="App">
-        <Router>
-          <h1>Server Sync Example</h1>
-          <InfoAnnotation />
-          <ServerSync />
-        </Router>
-      </div>
-    </GridContainer>
+    <Application>
+      <GridContainer>
+        <div className="App">
+          <Router>
+            <h1>Server Sync Example</h1>
+            <InfoAnnotation />
+            <ServerSync />
+          </Router>
+        </div>
+      </GridContainer>
+    </Application>
   );
 }
 
@@ -35,11 +35,7 @@ const InfoAnnotation = () => {
       <br />
       The data is cached in index db and you can view it by opening the application tab in the
       browser developer tools.
-      <Link
-        href="https://nmfs-radfish.github.io/radfish"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <Link href="https://nmfs-radfish.github.io/radfish" target="_blank" rel="noopener noreferrer">
         <Button type="button">Go To Documentation</Button>
       </Link>
     </Alert>
