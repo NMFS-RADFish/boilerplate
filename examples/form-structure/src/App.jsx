b100e79d52cb443c190f9bc1205818033b06e591
Jesse Copeland <jesse.copeland@noaa.gov>
2024-10-16 14:49:46 -1000

    Update to match structure of other examples

--- examples/form-structure/src/App.jsx
+++ examples/form-structure/src/App.jsx
@@ -1,26 +1,32 @@
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Alert, Button, Link } from "@trussworks/react-uswds";
+import { Alert, Button, Link, GridContainer } from "@trussworks/react-uswds";
+import { Application } from "@nmfs-radfish/react-radfish";
 
 import HomePage from "./pages/Home";
 
 function App() {
-  return (<div className="App grid-container">
-    <h1>Structured Form</h1>
-    <FormInfoAnnotation />
-    <br />
-    <Router>
-      <Routes>
-        <Route path="/" element={<FormPage />} />
-      </Routes>
-    </Router>
-  </div>
+  return (
+    <Application>
+      <GridContainer>
+        <div className="App grid-container">
+          <h1>Structured Form</h1>
+          <FormInfoAnnotation />
+          <br />
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </Router>
+        </div>
+      </GridContainer>
+    </Application>
   );
 }
 
 const FormInfoAnnotation = () => {
   return (
-    <Alert type="info" headingLevel={"h2"} heading="Information">
+    <Alert type="info" headingLevel={"h2"} heading="Information" validation>
       This example of building a non-trivial form using core components provided by <Link href="https://github.com/trussworks/react-uswds" target="_blank" rel="noopener noreferrer">Trussworks react-uswds</Link>. There are several useful elements that make up this example.
       <ul>
         <li>The Form structure is defined using the <em>Grid</em> components provided by <code>trussworks/react-uswds</code>.</li>
