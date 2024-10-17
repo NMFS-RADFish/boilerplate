3e5ffbe83c895e687134bc3c8af8f999c1c39878
Jesse Copeland <jesse.copeland@noaa.gov>
2024-10-16 14:49:04 -1000

    Remove unnecessary Application ref and SW files

--- examples/form-structure/src/index.jsx
+++ examples/form-structure/src/index.jsx
@@ -1,27 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ErrorBoundary } from "@nmfs-radfish/react-radfish";
 import "./styles/theme.css";
 import App from "./App";
-import { Application } from "@nmfs-radfish/radfish";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const app = new Application({
-  serviceWorker: {
-    url:
-      import.meta.env.MODE === "development"
-        ? "/mockServiceWorker.js"
-        : "/service-worker.js",
-  },
-  mocks: {
-    handlers: import("../mocks/browser.js"),
-  },
-});
-
-app.on("ready", () => {
-  root.render(
+root.render(
+  <ErrorBoundary>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
-  );
-});
+    </React.StrictMode>
+  </ErrorBoundary>
+);
