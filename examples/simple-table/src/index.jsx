c4e4ffd8c1d28fa163a4243e3210fa0367868912
Tony Gaskell <tony@sudokrew.com>
2024-12-16 15:02:19 -1000

    Remove on device storage wrapper from table example since it's not being used

--- examples/simple-table/src/index.jsx
+++ examples/simple-table/src/index.jsx
@@ -2,25 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/theme.css";
 import App from "./App";
-import { ErrorBoundary, OfflineStorageWrapper } from "@nmfs-radfish/react-radfish";
-
-const offlineStorageConfig = {
-  type: "indexedDB",
-  name: import.meta.env.VITE_INDEXED_DB_NAME,
-  version: import.meta.env.VITE_INDEXED_DB_VERSION,
-  stores: {
-    formData: "uuid, image, species, computedPrice, isDraft",
-  },
-};
-
+import { ErrorBoundary } from "@nmfs-radfish/react-radfish";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ErrorBoundary>
     <React.StrictMode>
-      <OfflineStorageWrapper config={offlineStorageConfig}>
-        <App />
-      </OfflineStorageWrapper>
+      <App />
     </React.StrictMode>
   </ErrorBoundary>,
 );
