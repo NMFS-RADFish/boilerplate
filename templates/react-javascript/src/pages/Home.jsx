08a797463efc75282f70e90236e5c5e9acd4800c
Jay Giang <jayhgiang@gmail.com>
2025-12-17 12:46:46 -0800

    feat(home): update HomePage to use RADFish configuration for logo

--- templates/react-javascript/src/pages/Home.jsx
+++ templates/react-javascript/src/pages/Home.jsx
@@ -2,23 +2,30 @@ import "../index.css";
 import React from "react";
 import { Button } from "@trussworks/react-uswds";
 import { Link } from "react-router-dom";
+import { useRadFishConfig } from "../hooks/useRadFishConfig.jsx";
 
 function HomePage() {
+  const config = useRadFishConfig();
+
   return (
-      <div className="display-flex flex-column flex-align-center">
-        <img src="/icons/radfish.png" alt="RADFish logo" height="200" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <Link
-            to="https://nmfs-radfish.github.io/radfish/developer-documentation/getting-started"
-            target="_blank"
-          >
-            <Button>Documentation</Button>
-          </Link>
-        </p>
-      </div>
+    <div className="display-flex flex-column flex-align-center">
+      <img
+        src={config.icons.logo}
+        alt={`${config.app.shortName} logo`}
+        height="200"
+      />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <p>
+        <Link
+          to="https://nmfs-radfish.github.io/radfish/developer-documentation/getting-started"
+          target="_blank"
+        >
+          <Button>Documentation</Button>
+        </Link>
+      </p>
+    </div>
   );
 }
 
